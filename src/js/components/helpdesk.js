46d6d69dbf14bea46e1257ee7f074d79bb6928d2
Alexander Pushkin (Home) <a.v.pushkin@gmail.com>
2023-10-31 18:50:26 +0300

    update

--- src/js/components/helpdesk.js
+++ src/js/components/helpdesk.js
@@ -1,48 +1,71 @@
-const subscribeWidget = document.querySelector('.subscribe')
-const subscribeForm = subscribeWidget.querySelector('.subscribe-form')
-const nameInput = subscribeWidget.querySelector('.name')
-const phoneInput = subscribeWidget.querySelector('.phone')
-const unsubscribeBtn = subscribeWidget.querySelector('.unsubscribe-btn')
+import { TicketManager } from './ticketManager'
+const btn = document.querySelector('.btn')
+const tickets = document.querySelector('.tickets')
 
-subscribeForm.addEventListener('submit', (e) => {
+document.addEventListener('DOMContentLoaded', (e) => {
   e.preventDefault()
-  const xhr = new XMLHttpRequest()
 
-  const body = Array.from(subscribeForm.elements)
-    .filter(({ name }) => name)
-    .map(({ name, value }) => `${name}=${encodeURIComponent(value)}`)
-    .join('&')
+  const xhr = new XMLHttpRequest()
 
   xhr.onreadystatechange = function () {
     if (xhr.readyState !== 4) return
-    console.log(xhr.responseText)
+
+    const data = JSON.parse(xhr.responseText)
+    data.forEach(el => {
+      const instance = new TicketManager(tickets, el)
+      instance.init()
+    })
   }
-
-  xhr.open('POST', 'http://localhost:6060')
-
-  xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
-
-  xhr.send(body)
-})
-
-unsubscribeBtn.addEventListener('click', (e) => {
-  e.preventDefault()
-  const xhr = new XMLHttpRequest()
-  console.log(Array.from(subscribeForm.elements).filter(({ name }) => name))
-
-  const body = Array.from(subscribeForm.elements)
-    .filter(({ name }) => name)
-    .map(({ name, value }) => `${name}=${encodeURIComponent(value)}`)
-    .join('&')
-
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState !== 4) return
-    console.log(xhr.responseText)
-  }
-
-  xhr.open('DELETE', 'http://localhost:6060/?' + body)
-
-  xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
-
+  xhr.open('GET', 'http://localhost:6060/?method=allTickets')
   xhr.send()
 })
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault()
+  const xhr = new XMLHttpRequest()
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== 4) return
+    console.log(JSON.parse(xhr.responseText))
+  }
+  xhr.open('GET', 'http://localhost:6060/?method=switchByID&id=1')
+  xhr.send()
+})
+
+// subscribeForm.addEventListener('submit', (e) => {
+//   e.preventDefault()
+
+//   const body = new FormData(subscribeForm)
+
+//   const xhr = new XMLHttpRequest()
+
+//   xhr.onreadystatechange = function () {
+//     if (xhr.readyState !== 4) return
+//     console.log(xhr.responseText)
+//   }
+
+//   xhr.open('POST', 'http://localhost:6060')
+
+//   xhr.send()
+// })
+
+// unsubscribeBtn.addEventListener('click', (e) => {
+//   e.preventDefault()
+//   const xhr = new XMLHttpRequest()
+
+//   const body = Array.from(subscribeForm.elements)
+//     .filter(({ name }) => name)
+//     .map(({ name, value }) => `${name}=${encodeURIComponent(value)}`)
+//     .join('&')
+
+//   xhr.onreadystatechange = function () {
+//     if (xhr.readyState !== 4) return
+//     console.log(xhr.responseText)
+//   }
+
+//   xhr.open('DELETE', 'http://localhost:6060/?' + body)
+
+//   xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
+
+//   xhr.send()
+// })
---
+++ src/js/components/ticketManager.js
@@ -0,0 +1,72 @@
+export class TicketManager {
+  constructor (taskDiv, tObject) {
+    this.taskBoard = taskDiv
+    this.ticket = tObject
+  }
+
+  init () {
+    this.renderTicket()
+
+    if (this.ticket.status === true) {
+      this.radioBtn.checked = true
+    } else {
+      this.radioBtn.checked = false
+    }
+
+    this.radioBtn.addEventListener('change', (e) => {
+      e.stopPropagation()
+      const xhr = new XMLHttpRequest()
+      xhr.open('GET', `http://localhost:6060/?method=switchByID&id=${this.ticket.id}`)
+      xhr.send()
+    })
+
+    this.ticketRendered.addEventListener('click', (e) => {
+      if (!e.target.closest('.noProp')) {
+        this.description.classList.toggle('hide')
+      }
+    })
+  }
+
+  renderTicket () {
+    this.taskBoard.insertAdjacentHTML('beforeend', this.htmlForm())
+  }
+
+  htmlForm () {
+    const { id, name, description, __, created } = this.ticket
+    const htmlRaw = `
+    <div class="ticket" id=${id}>
+      <div class="main-info">
+        <input type="checkbox" class="radio-button noProp">
+        <span class="name">${name}</span>
+        <span class="date">${created}</span>
+        <button class="delete-button noProp">X</button>
+        <button class="edit-button noProp">✏️</button>
+      </div>
+      <div class="additional-info">
+        <span class="description hide">${description}</span>
+      </div>
+    </div>
+    `
+    return htmlRaw
+  }
+
+  get ticketRendered () {
+    return document.getElementById(this.ticket.id)
+  }
+
+  get radioBtn () {
+    return this.ticketRendered.querySelector('.radio-button')
+  }
+
+  get delBtn () {
+    return this.ticketRendered.querySelector('.delete-button')
+  }
+
+  get editBtn () {
+    return this.ticketRendered.querySelector('.edit-button')
+  }
+
+  get description () {
+    return this.ticketRendered.querySelector('.description')
+  }
+}
